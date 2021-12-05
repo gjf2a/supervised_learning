c0c75bd259e8505d865eaf03f13d7ce4da973a60
gjf2a <ferrer@hendrix.edu>
2021-12-04 21:01:59 -0600

    Used KeyType to simplify generics.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,15 @@
-use hash_histogram::HashHistogram;
+use hash_histogram::{KeyType, HashHistogram};
 use std::{fmt, io};
-use std::fmt::{Debug, Display, Formatter};
+use std::fmt::{Display, Formatter};
 use std::collections::HashSet;
-use std::hash::Hash;
 use std::io::Write;
 
-pub struct ConfusionMatrix<L: Debug+Hash+Clone+Eq+Ord> {
+pub struct ConfusionMatrix<L: KeyType + Ord> {
     label_2_right: HashHistogram<L>,
     label_2_wrong: HashHistogram<L>,
 }
 
-impl <L: Debug+Hash+Clone+Eq+Ord> ConfusionMatrix<L> {
+impl <L: KeyType + Ord> ConfusionMatrix<L> {
     pub fn new() -> ConfusionMatrix<L> {
         ConfusionMatrix {
             label_2_right: HashHistogram::new(),
@@ -40,7 +39,7 @@ impl <L: Debug+Hash+Clone+Eq+Ord> ConfusionMatrix<L> {
     }
 }
 
-impl <L: Debug+Display+Hash+Clone+Eq+Ord> fmt::Display for ConfusionMatrix<L> {
+impl <L: KeyType + Ord + Display> fmt::Display for ConfusionMatrix<L> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let mut ordered_labels: Vec<L> = self.all_labels().iter().cloned().collect();
         ordered_labels.sort_unstable();
@@ -53,7 +52,7 @@ impl <L: Debug+Display+Hash+Clone+Eq+Ord> fmt::Display for ConfusionMatrix<L> {
     }
 }
 
-pub trait Classifier<I,L:Debug+Hash+Clone+Eq+Ord> {
+pub trait Classifier<I,L:KeyType + Ord> {
     fn train(&mut self, training_images: &Vec<(L,I)>);
 
     fn classify(&self, example: &I) -> L;
